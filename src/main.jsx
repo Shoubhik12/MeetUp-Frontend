f2b20c9585a6148cf12779dee2b8caffbc0b31a4
Shoubhik Ghosh <shoubhikghosh360@gmail.com>
2025-08-31 15:22:18 +0530

    feat: new file added

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Details from './pages/details.jsx'
+import Details from "./pages/Details.jsx"
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 
 const router = createBrowserRouter([
