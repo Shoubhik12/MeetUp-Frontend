04f973109e7d860829615a50ceaa8ca369a476dc
Shoubhik Ghosh <shoubhikghosh360@gmail.com>
2025-09-01 11:49:15 +0530

    feat- responsiveness added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import { useState } from 'react'
 import useFetch from "./useFetch"
 
 function App() {
   
   const [search, setSearch] = useState("")
-  const [type, setType] = useState("")
+  const [type, setType] = useState("Both")
   const [tag , setTags] = useState("")
 
   const {data,loading,error} = useFetch("https://meet-up-backend-eight.vercel.app/meets")
@@ -19,8 +20,8 @@ function App() {
      console.log(filteredData)
   }
 
-  if(!loading && type!=""){
-     filteredData = type!=""?data.filter((dat)=>dat.eventType==type) :data
+  if(!loading && type!="Both"){
+     filteredData = type!="Both"?data.filter((dat)=>dat.eventType==type) :data
   }
 
   return (
@@ -30,21 +31,32 @@ function App() {
          <hr />
       </header>
       <main className="px-2 ">
-          <input type="text" placeholder="Search By Title " id="search" className="form-control" onChange={(event)=>setSearch(event.target.value)} />
-          <input type="text" placeholder="Search By Tags" id="tags" className="form-control" onChange={(event)=>setTags(event.target.value)} />
-          <select id="eventType"  className="form-select" onChange={(event)=>setType(event.target.value)}  >
-              <option >Search By Type</option>
-              <option value="Offline">Offline</option>
-              <option value="Online">Online</option>
+          <div className="row g-3 mb-4">
+              <div className="col-12 col-md-4">
+                    <input type="text" placeholder="Search By Title " id="search" className="form-control" onChange={(event)=>setSearch(event.target.value)} />
+              </div>
+              <div className="col-12 col-md-4">
+                    <input type="text" placeholder="Search By Tags" id="tags" className="form-control" onChange={(event)=>setTags(event.target.value)} />
+              </div>
+              <div className="col-12 col-md-4 ">
+                       <select id="eventType"  className="form-select" onChange={(event)=>setType(event.target.value)}  >
+                       <option value="Both" >Both</option>
+                       <option value="Offline">Offline</option>
+                       <option value="Online">Online</option>
           </select>
-          <div className="  py-4" style={{width:"1000px",paddingLeft:"500px"}}>
+              </div>
+          </div>  
+          <div className=" row py-4 mx-3" >
              { !loading && filteredData.map((data)=>(
-                <div className="card px-3 py-3 my-3 ">
-                      <img src={data.img} alt={`img${data.title}`} />
-                      <h4>{data.title}</h4>
-                      <p>{data.dateAndTimings} <br /> <strong>{data.eventType}</strong> </p>
-                      <a href={`meet/${data._id}`}  className="btn btn-primary">View Details</a>
-                </div>
+                 <div className="col-12 col-sm-6 col-md-4 mb-4 ">
+                       <div className="  card  px-3 py-3 my-3 center-align " style={{height:"500px"}} >
+                          <img src={data.img} alt={`img${data.title}`} height={"300px"}  />
+                          <h4>{data.title}</h4>
+                          <p>{data.dateAndTimings} <br /> <strong>{data.eventType}</strong> </p>
+                          <a href={`meet/${data._id}`}  className="btn btn-primary">View Details</a>
+                      </div>
+                 </div>
+                
                ))
              }
           </div>
